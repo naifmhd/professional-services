460ec058012ab1686f76d88926d6749d279a55dc
Naif Mohamed <naifmhd@gmail.com>
2020-02-29 22:34:03 +0500

    d

--- examples/cloudml-document-ai-death/training_utils.py
+++ examples/cloudml-document-ai-death/training_utils.py
@@ -502,8 +502,8 @@ def run_ocr(project_id, output_directory, region, temp_directory, service_acct):
             temp_txt = os.path.join(
                 temp_directory, os.path.basename(blob.name).replace(".png", ".txt"))
 
-            with open(temp_txt, "w") as f:
-                f.write(text.encode('utf8'))
+            with open(temp_txt, "w", encoding="utf-8") as f:
+                f.write(text)
                 f.close()
 
     subprocess.run(
